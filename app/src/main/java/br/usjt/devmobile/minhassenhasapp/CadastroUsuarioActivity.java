b5d92a3aed1370bf8941ad14fcfe582290e01ac0
marciafs <49418173+marciafs@users.noreply.github.com>
2020-04-02 01:07:38 -0300

    final commit

--- app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroUsuarioActivity.java
+++ app/src/main/java/br/usjt/devmobile/minhassenhasapp/CadastroUsuarioActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 import com.google.android.material.textfield.TextInputEditText;
 import com.orhanobut.hawk.Hawk;
 
+import java.util.List;
+
 public class CadastroUsuarioActivity extends AppCompatActivity {
 
     private static final String TAG = "CadastroUsuarioActivity";
@@ -18,31 +20,31 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
     private TextInputEditText pergunta;
     private TextInputEditText resposta;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
-        usuario = (TextInputEditText) findViewById(R.id.usuarioEditTextInput);
-        senha = (TextInputEditText) findViewById(R.id.senhaEditTextInput);
-        pergunta = (TextInputEditText) findViewById(R.id.perguntaEditTextInput);
-        resposta = (TextInputEditText) findViewById(R.id.respostaEditTextInput);
-        Hawk.init( this).build();
-
+        usuario = (TextInputEditText)findViewById(R.id.usuarioEditTextInput);
+        senha = (TextInputEditText)findViewById(R.id.senhaEditTextInput);
+        pergunta = (TextInputEditText)findViewById(R.id.perguntaEditTextInput);
+        resposta = (TextInputEditText)findViewById(R.id.respostaEditTextInput);
+        Hawk.init(this).build();
     }
 
-    public void cadastrarUsuario(View view) {
-        //TODO fazer o cadastro
-        Log.d(TAG, "Clicou no fazer cadastro!");
+    public void cadastrarUsuario(View view){
+        // TODO fazer o cadastro
+        Log.d(TAG,"Clicou no fazer cadastro!");
 
         Hawk.put("usuario", usuario.getText().toString());
         Hawk.put("senha", senha.getText().toString());
         Hawk.put("pergunta", pergunta.getText().toString());
         Hawk.put("resposta", resposta.getText().toString());
 
-        Toast.makeText(this, "Cadastro Realizado com Sucesso", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Cadastro Realizado com Sucesso",Toast.LENGTH_LONG).show();
 
         finish();
 
     }
+
+
 }
